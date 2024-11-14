a9e67c5f874828f99b4b4981516dca7d6f4936d1
isatvikmishra <asksatvik@gmail.com>
2024-11-14 17:22:58 +0530

    added shadow

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   return (
     
     <div className='flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 border-red-400 p-20 rounded-xl'>
+        <div className='border-2 border-red-400 shadow-lg rounded-lg p-20 rounded-xl  '>
             <form
             onSubmit={(e)=>{
                 submitHandler(e)
@@ -30,7 +30,7 @@ const Login = () => {
 
                 required
 
-                className='text-lg text-white outline-none font-medium bg-transparent placeholder:text-gray-300 px-6 py-2 border-2 border-red-400 rounded-full' type="email" placeholder='Email'/>
+                className='text-lg text-white outline-none font-medium bg-transparent placeholder:text-gray-300 px-6 py-2 border-2 border-red-400 rounded-full  focus:border-2 focus:border-red-500 transition duration-200' type="email" placeholder='Email'/>
                 
             <input
                 value={password}
@@ -38,8 +38,8 @@ const Login = () => {
                     setPassword(e.target.value)
                 }}
                 required
-                className='text-lg text-white outline-none font-medium bg-transparent placeholder:text-gray-300 px-6 py-2 border-2 mt-4 border-red-400 rounded-full' type="password" placeholder='Password'/>
-                <button className='w-full rounded-full mt-7 px-8 py-2 text-lg text-white bg-red-400  hover:bg-red-500 font-semibold '>Login</button>
+                className='text-lg text-white outline-none font-medium bg-transparent placeholder:text-gray-300 px-6 py-2 border-2 mt-4 border-red-400 rounded-full focus:border-2 focus:border-red-500 transition duration-200' type="password" placeholder='Password'/>
+                <button className='w-full rounded-full mt-7 px-8 py-2 text-lg shadow-md shadow-zinc-600/50 hover:shadow-zinc-800/50	 text-white bg-red-400  hover:bg-red-500 font-semibold '>Login</button>
             </form>
         </div>
 
